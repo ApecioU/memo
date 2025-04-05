dd965e186d22b792eb807af1e74a72304c4b8390
apecio <pescasioaaron92@gmail.com>
2025-04-05 19:56:02 +0200

    Last updated date test commit revert

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -2,18 +2,10 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
-
 export default function Layout({ children }: { children: ReactNode }) {
-  const currentPage = source.getCurrentPage();
-  const path = currentPage?.file.path;
-
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-      
-      <div className="mt-4 text-sm text-muted-foreground">
-        Last updated: {new Date(currentPage?.data.lastModified ?? '').toLocaleDateString()}
-      </div>
-    </DocsLayout>
-  );
+return (
+<DocsLayout tree={source.pageTree} {...baseOptions}>
+{children}
+</DocsLayout>
+);
 }
